4b0a16f6238086945791a9b76d35a94746d72441
Rendyl93 <122086822+Rendyl93@users.noreply.github.com>
2023-04-11 14:29:07 +0200

    Add files via upload

---
+++ phone-shop/mvc.sql
@@ -0,0 +1,230 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 10 mars 2023 à 16:55
+-- Version du serveur : 10.4.27-MariaDB
+-- Version de PHP : 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `mvc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `accessoire`
+--
+
+CREATE TABLE `accessoire` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prix` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `achat`
+--
+
+CREATE TABLE `achat` (
+  `id` int(11) NOT NULL,
+  `id_telephone` int(11) NULL,
+  `id_commande` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `id_accessoire` int(11) NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `achat`
+--
+
+INSERT INTO `achat` (`id`, `id_telephone`, `id_commande`, `quantity`, `id_accessoire`) VALUES
+(1, 4, 1, 6, NULL),
+(2, 5, 1, 7, NULL),
+(3, 5, 2, 4, NULL),
+(4, 4, 2, 7, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `avis`
+--
+
+CREATE TABLE `avis` (
+  `id` int(11) NOT NULL,
+  `id_telephone` int(11) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  `commentaire` text NOT NULL,
+  `notes` int(11) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+CREATE TABLE `commande` (
+  `id` int(11) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `statut` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `commande`
+--
+
+INSERT INTO `commande` (`id`, `id_utilisateur`, `date`, `statut`) VALUES
+(1, 1, '2023-03-09 12:36:23', 'Envoyé'),
+(2, 1, '2023-03-09 12:36:26', 'Reçu');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `telephone`
+--
+
+CREATE TABLE `telephone` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prix` float NOT NULL,
+  `stockage` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `telephone`
+--
+
+INSERT INTO `telephone` (`id`, `nom`, `prix`, `stockage`, `description`, `quantity`, `image`) VALUES
+(4, 'Samsung s23', 1500, '512Go', 'super tel qui peut tout faire ', 21, '06032023134601s23.jpg'),
+(5, 'iphone', 1800, '256Go', 'il est chère et pas ouf', 19, '06032023142215iphone.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE `utilisateurs` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `age` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mdp` varchar(255) NOT NULL,
+  `pseudo` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `age`, `email`, `mdp`, `pseudo`, `role`) VALUES
+(1, 'admin', 'admin', 23, 'admin@admin.com', '$2y$10$.QziHljF/b0bQDG/Owbj2etBRIzvNXDllWjOA3sdvf1ckRThcABYy', 'admin', 'ROLE_ADMIN');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `accessoire`
+--
+ALTER TABLE `accessoire`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `achat`
+--
+ALTER TABLE `achat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `avis`
+--
+ALTER TABLE `avis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `telephone`
+--
+ALTER TABLE `telephone`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `accessoire`
+--
+ALTER TABLE `accessoire`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `achat`
+--
+ALTER TABLE `achat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `avis`
+--
+ALTER TABLE `avis`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `commande`
+--
+ALTER TABLE `commande`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `telephone`
+--
+ALTER TABLE `telephone`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
